ca54faca9ed5a1d39c5f882035c4e3bc0b86144d
Srisaketh Peddi <srisaketh.peddi@gmail.com>
2024-08-07 23:09:13 -0400

    Your descriptive commit message

--- app/page.js
+++ app/page.js
@@ -37,35 +37,34 @@ export default function Home() {
       { role: "user", content: message },
       { role: "assistant", content: `` },
     ]);
-    const response = fetch('/api/chat', {
+    const response = await fetch('/api/chat', {
       method: "POST",
       headers: {
         'Content-Type': 'application/json'
       },
       body: JSON.stringify([...messages, { role: 'user', content: message }])
-    }).then(async (res) => {
-      const reader = res.body.getReader();
-      const decoder = new TextDecoder();
+    });
+    const reader = response.body.getReader();
+    const decoder = new TextDecoder();
 
-      let result = '';
-      return reader.read().then(function processText({ done, value }) {
-        if (done) {
-          return result;
-        }
-        const text = decoder.decode(value || new Int8Array(), { stream: true });
-        setMessages((messages) => {
-          let lastMessage = messages[messages.length - 1];
-          let otherMessages = messages.slice(0, messages.length - 1);
-          return [
-            ...otherMessages,
-            {
-              ...lastMessage,
-              content: lastMessage.content + text
-            },
-          ];
-        });
-        return reader.read().then(processText);
+    let result = '';
+    return reader.read().then(function processText({ done, value }) {
+      if (done) {
+        return result;
+      }
+      const text = decoder.decode(value || new Int8Array(), { stream: true });
+      setMessages((messages) => {
+        let lastMessage = messages[messages.length - 1];
+        let otherMessages = messages.slice(0, messages.length - 1);
+        return [
+          ...otherMessages,
+          {
+            ...lastMessage,
+            content: lastMessage.content + text
+          },
+        ];
       });
+      return reader.read().then(processText);
     });
   };
 
